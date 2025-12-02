69ea487784901958600661cd3e463f03e024adb7
Jonade Naeem <jonade.naeem@gmail.com>
2025-12-02 15:38:56 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+# Entry point: runs the full pipeline (preprocessing, training, evaluation)
+
+# Import function and its subfunctions from the preprocess_audio.py
+from preprocess_audio import extract_and_save_mel_features
+from preprocess_audio import midi_to_targets
+import numpy as np
+import os
+import glob
+
+
+# =======================
+# Preprocessing
+# =======================
+
+# The following will execute when we call main.py in python
+if __name__ == "__main__":
+
+    # Processes audio file to npz
+    # Where we get audio from
+    #audio_dir = "/Users/jonade/code/nompar/piano_ai/raw_data/2017"  # Folder with your 2017 audio files
+    # Where we save it to
+    #out_dir = "/Users/jonade/code/nompar/piano_ai/2017_npz"  # Output folder for processed features
+    #extract_and_save_mel_features(audio_dir, out_dir)
+
+    # Process one MIDI file to targets
+    midi_dir = "/Users/jonade/code/nompar/piano_ai/raw_data/2017"  # Path to your MIDI file
+    out_dir = "/Users/jonade/code/nompar/piano_ai/2017_midi_targets_npz"
+
+    # Get a list of midi names from the raw data directory
+    midi_files = sorted(glob.glob(os.path.join(midi_dir, "*.mid")))
+
+    # Loop through each midi path and pass it to midi_to_targets
+    for midi_path in midi_files:
+        print(f"Processing {midi_path}")  # <-- Add this line
+        name = os.path.splitext(os.path.basename(midi_path))[0]
+
+        n_frames = 100  # Set this to the number of frames you want (should match your mel features)
+
+        # Get the 5 targets
+        onset, offset, active, velocity, pedal = midi_to_targets(midi_path, n_frames)
+
+        # Save the targets for later use
+        np.savez_compressed(
+            os.path.join(out_dir, f"{name}_targets.npz"),
+            onset=onset,
+            offset=offset,
+            active=active,
+            velocity=velocity,
+            pedal=pedal
+            )

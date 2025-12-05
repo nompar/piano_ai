01d2b0962d3c2e0b1ac6d9e586258ec0dd7b09ce
Jonade Naeem <jonade.naeem@gmail.com>
2025-12-05 10:42:15 +0100

    added the new chunk functions to init to ensure they are called from piano_ai and not .preprocess_audio.py

--- piano_ai/__init__.py
+++ piano_ai/__init__.py
@@ -12,6 +12,9 @@ from .preprocess_audio import (
     extract_and_save_mel_features,
     midi_to_targets,
     audio_to_mel_3d,
+    midi_to_targets_chunks,
+    chunk_mel,
+
     # Add new functions from preprocess_audio.py here
 )
 
